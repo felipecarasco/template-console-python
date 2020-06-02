ccdc3921761bc3e8dd30dd09a04f09b434c388e1
Felipe Carasco <felipe@carasco.com.br>
2020-06-01 21:55:55 -0300

    Commit inicial
    Commit inicial do template para projetos console

---
+++ template_console/__init__.py
@@ -0,0 +1,1 @@
+from .template_console import TemplateConsole
---
+++ template_console/template_console.py
@@ -0,0 +1,6 @@
+class TemplateConsole(object):
+    def __init__(self):
+        pass
+
+    def run(self):
+        print("Hello World")
